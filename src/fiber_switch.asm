4f06c4e21cbc71f3cd586385d84bf4acb90d867e
Supercip971 <supercyp971@gmail.com>
2023-02-13 11:03:57 -0400

    style: tyding up fiber switch assembly code

--- src/fiber_switch.asm
+++ src/fiber_switch.asm
@@ -18,33 +18,36 @@ global fibers_switch
 fibers_switch:
     ; SAVING 
     mov r8, [rsp]
-    mov [rdi] , r8 ; from->sp = rsp
+    mov [rdi + fibers_ctx.ip] , r8 ; from->sp = rsp
 
     lea r8, [rsp+8] ; r8 = rsp + 8 (return address)
-    mov [rdi+8], r8 ; from->sp = rsp + 8
+    mov [rdi+ fibers_ctx.sp], r8 ; from->sp = rsp + 8
 
 
-    mov [rdi+16], rbx ; from->rbx = rbx
-    mov [rdi+24], rbp ; from->rbp = rbp
-    mov [rdi+32], r12 ; from->r12 = r12 
-    mov [rdi+40], r13 ; from->r13 = r13
-    mov [rdi+48], r14  ; from->r14 = r14 
-    mov [rdi+56], r15 ; from->r15 = r15 
-    stmxcsr [rdi+64] ; from->mxcsr = mxcsr
-    fnstcw [rdi+68] ; from->x86_fcw = x86_fcw
+    mov [rdi + fibers_ctx.rbx]  , rbx ; from->rbx = rbx
+    mov [rdi + fibers_ctx.rbp]  , rbp ; from->rbp = rbp
+    mov [rdi + fibers_ctx.r12]  , r12 ; from->r12 = r12 
+    mov [rdi + fibers_ctx.r13]  , r13 ; from->r13 = r13
+    mov [rdi + fibers_ctx.r14]  , r14 ; from->r14 = r14 
+    mov [rdi + fibers_ctx.r15]  , r15 ; from->r15 = r15 
 
-    mov rsp, [rsi+8] ; rsp = to->sp
+    stmxcsr [rdi + fibers_ctx.mxcsr] ; from->mxcsr = mxcsr
+    fnstcw  [rdi + fibers_ctx.x86_fcw] ; from->x86_fcw = x86_fcw
 
-    mov rbx, [rsi+16] ; rbx = to->rbx
-    mov rbp, [rsi+24] ; rbp = to->rbp
-    mov r12, [rsi+32] ; r12 = to->r12
-    mov r13, [rsi+40] ; r13 = to->r13 
-    mov r14, [rsi+48] ; r14 = to->r14 
-    mov r15, [rsi+56] ; r15 = to->r15
+    ; LOADING
+    mov rsp, [rsi + fibers_ctx.sp] ; rsp = to->sp
 
-    ldmxcsr [rsi+64] ; mxcsr = to->mxcsr
-    fldcw [rsi+68] ; x86_fcw = to->x86_fcw
-    mov r8, [rsi] ; r8 = to->ip
+    mov rbx, [rsi + fibers_ctx.rbx] ; rbx = to->rbx
+    mov rbp, [rsi + fibers_ctx.rbp] ; rbp = to->rbp
+    mov r12, [rsi + fibers_ctx.r12] ; r12 = to->r12
+    mov r13, [rsi + fibers_ctx.r13] ; r13 = to->r13 
+    mov r14, [rsi + fibers_ctx.r14] ; r14 = to->r14 
+    mov r15, [rsi + fibers_ctx.r15] ; r15 = to->r15
+
+    ldmxcsr  [rsi + fibers_ctx.mxcsr] ; mxcsr = to->mxcsr
+    fldcw    [rsi + fibers_ctx.x86_fcw] ; x86_fcw = to->x86_fcw
+
+    mov r8,  [rsi + fibers_ctx.ip] ; r8 = to->ip
     push r8 ; stack[] = to->ip 
 
     ret ; ip = stack[]
